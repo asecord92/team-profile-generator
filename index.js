e08c67cf02b20d8d2495ce2fe9ca8777f23d8cbf
Adam <ats92@cox.net>
2021-10-09 21:00:25 -0700

    added prompts for engineer and intern

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
 const { choices } = require('yargs');
 const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 const team = [];
 
@@ -14,7 +16,7 @@ function managerPrompt() {
     {
         type:'text',
         name: 'id',
-        message:"Please enter your employee ID:"
+        message:"Please enter their employee ID:"
     },    
     {
         type:'text',
@@ -30,7 +32,7 @@ function managerPrompt() {
     .then(({ name, id, email, officeNumber }) => {
         const manager = new Manager(name, id, email, officeNumber)
         team.push(manager);
-        console.log(team);
+        roleSelect();
     });
     
 
@@ -42,17 +44,75 @@ function roleSelect() {
         type: 'list',
         name: 'role',
         message: 'What employee would you like to add?',
-        choices: ['Engineer', 'Intern']
+        choices: ['Engineer', 'Intern', 'Finish']
     }])
     .then(teamMember => {
         if (teamMember.role === 'Engineer') {
             engineerPrompt();
+        }else if (teamMember.role === 'Intern') {
+            internPrompt();
         }
-    })
-        
-    
+    }) 
 }
 
+function engineerPrompt() {
+    inquirer.prompt([
+        {
+            type:'text',
+            name: 'name',
+            message:"Intern Name"
+        },
+        {
+            type:'text',
+            name: 'id',
+            message:"Please enter their employee ID:"
+        },    
+        {
+            type:'text',
+            name: 'email',
+            message:"Please enter their email:"
+        },
+        {
+            type: 'text',
+            name: 'github',
+            message: 'Please enter their github username'
+        }])
+    .then(({ name, id, email, github }) =>{
+        const engineer = new Intern(name, id, email, github)
+        team.push(engineer);
+        roleSelect();
+
+    })
+}
+
+function internPrompt() {
+    inquirer.prompt([
+        {
+            type:'text',
+            name: 'name',
+            message:"Intern Name"
+        },
+        {
+            type:'text',
+            name: 'id',
+            message:"Please enter your employee ID:"
+        },    
+        {
+            type:'text',
+            name: 'email',
+            message:"Please enter their email:"
+        },{
+            type: 'text',
+            name: 'school',
+            message: 'Please enter what school they are studying at:'
+        }])
+    .then(({ name, id, email, school }) =>{
+        const intern = new Intern(name, id, email, school)
+        team.push(intern);
+        roleSelect();
+
+    })
+}
 managerPrompt();
 
 
